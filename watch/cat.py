99115d6224158e47892606a3a73375472dfb74d1
XinYao1994 <xinyaoforwork@gmail.com>
2019-06-29 15:55:05 +0800

    add horse example

---
+++ watch/cat.py
@@ -0,0 +1,266 @@
+import os
+import zipfile
+
+local_zip = './watch/data/cats_and_dogs_filtered.zip'
+
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+
+zip_ref.extractall('./watch/data/')
+zip_ref.close()
+
+base_dir = './watch/data/cats_and_dogs_filtered'
+
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+# Directory with our training cat/dog pictures
+train_cats_dir = os.path.join(train_dir, 'cats')
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+
+# Directory with our validation cat/dog pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+train_cat_fnames = os.listdir(train_cats_dir)
+train_dog_fnames = os.listdir(train_dogs_dir)
+
+print(train_cat_fnames[:10])
+print(train_dog_fnames[:10])
+
+print('total training cat images :', len(os.listdir(train_cats_dir)))
+print('total training dog images :', len(os.listdir(train_dogs_dir)))
+
+print('total validation cat images :', len(os.listdir(validation_cats_dir)))
+print('total validation dog images :', len(os.listdir(validation_dogs_dir)))
+
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+# Parameters for our graph; we'll output images in a 4x4 configuration
+nrows = 4
+ncols = 4
+
+pic_index = 0 # Index for iterating over images
+
+'''
+fig = plt.gcf()
+fig.set_size_inches(ncols*4, nrows*4)
+
+pic_index+=8
+
+next_cat_pix = [os.path.join(train_cats_dir, fname) 
+                for fname in train_cat_fnames[ pic_index-8:pic_index] 
+               ]
+
+next_dog_pix = [os.path.join(train_dogs_dir, fname) 
+                for fname in train_dog_fnames[ pic_index-8:pic_index]
+               ]
+
+for i, img_path in enumerate(next_cat_pix+next_dog_pix):
+  # Set up subplot; subplot indices start at 1
+  sp = plt.subplot(nrows, ncols, i + 1)
+  sp.axis('Off') # Don't show axes (or gridlines)
+
+  img = mpimg.imread(img_path)
+  plt.imshow(img)
+
+plt.show()
+'''
+import tensorflow as tf
+import keras
+
+model = keras.models.Sequential([
+    # Note the input shape is the desired size of the image 150x150 with 3 bytes color
+    keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150, 150, 3)),
+    keras.layers.MaxPooling2D(2,2),
+    keras.layers.Conv2D(32, (3,3), activation='relu'),
+    keras.layers.MaxPooling2D(2,2), 
+    keras.layers.Conv2D(64, (3,3), activation='relu'), 
+    keras.layers.MaxPooling2D(2,2),
+    # Flatten the results to feed into a DNN
+    keras.layers.Flatten(), 
+    # 512 neuron hidden layer
+    keras.layers.Dense(512, activation='relu'), 
+    # Only 1 output neuron. It will contain a value from 0-1 where 0 for 1 class ('cats') and 1 for the other ('dogs')
+    keras.layers.Dense(1, activation='sigmoid')  
+])
+
+'''
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+'''
+
+model.summary()
+
+from keras.optimizers import RMSprop
+
+model.compile(optimizer=RMSprop(lr=0.001),
+              loss='binary_crossentropy',
+              metrics = ['acc'])
+
+from keras.preprocessing.image import ImageDataGenerator
+
+# All images will be rescaled by 1./255.
+# train_datagen = ImageDataGenerator(
+#      rescale = 1.0/255.)
+
+train_datagen = ImageDataGenerator(
+      rescale=1./255,
+      rotation_range=40,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      shear_range=0.2,
+      zoom_range=0.2,
+      horizontal_flip=True,
+      fill_mode='nearest')
+
+test_datagen  = ImageDataGenerator(rescale = 1.0/255.)
+
+# --------------------
+# Flow training images in batches of 20 using train_datagen generator
+# --------------------
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    batch_size=20,
+                                                    class_mode='binary',
+                                                    target_size=(150, 150))     
+# --------------------
+# Flow validation images in batches of 20 using test_datagen generator
+# --------------------
+validation_generator =  test_datagen.flow_from_directory(validation_dir,
+                                                         batch_size=20,
+                                                         class_mode  = 'binary',
+                                                         target_size = (150, 150))
+
+history = model.fit_generator(train_generator,
+                              validation_data=validation_generator,
+                              steps_per_epoch=100,
+                              epochs=15,
+                              validation_steps=50,
+                              verbose=2)
+
+import numpy as np
+import random
+from   keras.preprocessing.image import img_to_array, load_img
+
+# Let's define a new Model that will take an image as input, and will output
+# intermediate representations for all layers in the previous model after
+# the first.
+successive_outputs = [layer.output for layer in model.layers[1:]]
+
+#visualization_model = Model(img_input, successive_outputs)
+visualization_model = keras.models.Model(inputs = model.input, outputs = successive_outputs)
+
+# Let's prepare a random input image of a cat or dog from the training set.
+cat_img_files = [os.path.join(train_cats_dir, f) for f in train_cat_fnames]
+dog_img_files = [os.path.join(train_dogs_dir, f) for f in train_dog_fnames]
+
+img_path = random.choice(cat_img_files + dog_img_files)
+img = load_img(img_path, target_size=(150, 150))  # this is a PIL image
+
+x   = img_to_array(img)                           # Numpy array with shape (150, 150, 3)
+x   = x.reshape((1,) + x.shape)                   # Numpy array with shape (1, 150, 150, 3)
+
+# Rescale by 1/255
+x /= 255.0
+
+# Let's run our image through our network, thus obtaining all
+# intermediate representations for this image.
+successive_feature_maps = visualization_model.predict(x)
+
+# These are the names of the layers, so can have them as part of our plot
+layer_names = [layer.name for layer in model.layers]
+
+# -----------------------------------------------------------------------
+# Now let's display our representations
+# -----------------------------------------------------------------------
+for layer_name, feature_map in zip(layer_names, successive_feature_maps):
+  
+  if len(feature_map.shape) == 4:
+    
+    #-------------------------------------------
+    # Just do this for the conv / maxpool layers, not the fully-connected layers
+    #-------------------------------------------
+    n_features = feature_map.shape[-1]  # number of features in the feature map
+    size       = feature_map.shape[ 1]  # feature map shape (1, size, size, n_features)
+    
+    # We will tile our images in this matrix
+    display_grid = np.zeros((size, size * n_features))
+    
+    #-------------------------------------------------
+    # Postprocess the feature to be visually palatable
+    #-------------------------------------------------
+    for i in range(n_features):
+      x  = feature_map[0, :, :, i]
+      x -= x.mean()
+      x /= x.std ()
+      x *=  64
+      x += 128
+      x  = np.clip(x, 0, 255).astype('uint8')
+      display_grid[:, i * size : (i + 1) * size] = x # Tile each filter into a horizontal grid
+
+    #-----------------
+    # Display the grid
+    #-----------------
+
+    scale = 20. / n_features
+    plt.figure( figsize=(scale * n_features, scale) )
+    plt.title ( layer_name )
+    plt.grid  ( False )
+    plt.imshow( display_grid, aspect='auto', cmap='viridis' ) 
+
+# import os, signal
+
+# os.kill(os.getpid(), signal.SIGKILL)
+
+'''
+
+def split_data(SOURCE, TRAINING, TESTING, SPLIT_SIZE):
+    files = []
+    for filename in os.listdir(SOURCE):
+        file = SOURCE + filename
+        if os.path.getsize(file) > 0:
+            files.append(filename)
+        else:
+            print(filename + " is zero length, so ignoring.")
+
+    training_length = int(len(files) * SPLIT_SIZE)
+    testing_length = int(len(files) - training_length)
+    shuffled_set = random.sample(files, len(files))
+    training_set = shuffled_set[0:training_length]
+    testing_set = shuffled_set[-testing_length:]
+
+    for filename in training_set:
+        this_file = SOURCE + filename
+        destination = TRAINING + filename
+        copyfile(this_file, destination)
+
+    for filename in testing_set:
+        this_file = SOURCE + filename
+        destination = TESTING + filename
+        copyfile(this_file, destination)
+
+
+CAT_SOURCE_DIR = "/tmp/PetImages/Cat/"
+TRAINING_CATS_DIR = "/tmp/cats-v-dogs/training/cats/"
+TESTING_CATS_DIR = "/tmp/cats-v-dogs/testing/cats/"
+DOG_SOURCE_DIR = "/tmp/PetImages/Dog/"
+TRAINING_DOGS_DIR = "/tmp/cats-v-dogs/training/dogs/"
+TESTING_DOGS_DIR = "/tmp/cats-v-dogs/testing/dogs/"
+
+split_size = .9
+split_data(CAT_SOURCE_DIR, TRAINING_CATS_DIR, TESTING_CATS_DIR, split_size)
+split_data(DOG_SOURCE_DIR, TRAINING_DOGS_DIR, TESTING_DOGS_DIR, split_size)
+
+'''
+
