7d7bbed01b5698fa69d74b6a13f6308c287fa6c4
XinYao1994 <xyao@cs.hku.hk>
2022-09-29 22:56:29 +0800

    Updated codes.

--- vision/draw/clip/stable-diffusion.py
+++ vision/draw/clip/stable-diffusion.py
@@ -10,10 +10,15 @@ device = "cuda"
 # scheduler = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000)
 # pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, use_auth_token=True)
 
-pipe = StableDiffusionPipeline.from_pretrained(model_id, revision="fp16", use_auth_token=True)
+pipe = StableDiffusionPipeline.from_pretrained(model_id, use_auth_token=True)
 pipe = pipe.to(device)
 
-prompt = "a photo of an astronaut riding a horse on mars"
+# prompt = "an astronaut riding a cartoon horse on mars"
+
+# prompt = "a design of shopping mall with water street."
+
+prompt = "celebrating Festival at night with full moon."
+
 with autocast("cuda"):
     image = pipe(prompt, guidance_scale=7.5)["sample"][0]  
     
